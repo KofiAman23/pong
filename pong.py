551a057b98cc9a7c9fe062dac0455ddd0152c7d7
KofiAman23 <112861583+KofiAman23@users.noreply.github.com>
2023-01-28 19:43:41 -0500

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,104 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("kofi Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+score_a = 0
+score_b = 0
+
+
+paddle_a = turtle.Turtle()
+paddle_a.speed()
+paddle_a.shape("square")
+paddle_a.color("grey")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+paddle_b = turtle.Turtle()
+paddle_b.speed()
+paddle_b.shape("square")
+paddle_b.color("grey")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("grey")
+ball.penup()
+ball.goto(0,0)
+ball.dx =2
+ball.dy =-2
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("grey")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("player A: 0 playerB: 0", align="center", font=("Courier", 24, "normal"))
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+def paddle_b_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+wn.listen()
+wn.onkeypress(paddle_a_up, "a")
+wn.onkeypress(paddle_a_down, "z")
+wn.onkeypress(paddle_b_up, "k")
+wn.onkeypress(paddle_b_down, "m")
+
+while True:
+      wn.update()
+
+      ball.setx(ball.xcor() + ball.dx)
+      ball.sety(ball.ycor() + ball.dy)
+
+      if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+      if ball.ycor() < -290:
+         ball.sety(-290)
+         ball.dy *= -1
+
+      if ball.xcor() > 390:
+          ball.goto(0, 0)
+          ball.dx *= -1
+          score_a += 1
+          pen.clear()
+          pen.write("player A: {} playerB: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+      if ball.xcor() < -390:
+          ball.goto(0, 0)
+          ball.dx *= -1
+          score_b +=1
+          pen.clear()
+          pen.write("player A: {} playerB: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+      if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -50):
+          ball.setx(340)
+          ball.dx *= -1
+
+      if (ball.xcor() > -340 and ball.xcor() < -350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -50):
+          ball.setx(-340)
+          ball.dx *= -1
